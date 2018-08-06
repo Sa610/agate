21a32952303230fb3e169aef23fa29ab593a1d3c
Sa610 <salvatore.piazzolla.03@gmail.com>
2018-08-06 02:19:10 +0200

    refining active_model

--- app/controllers/home_controller.ts
+++ app/controllers/home_controller.ts
@@ -4,18 +4,13 @@ import ApplicationController    from "./application_controller";
 import Article                  from '../models/article';
 
 export default class HomeController extends ApplicationController {
-    public welcome(): void {
+    public async welcome() {
         this.scope.message  = 'Welcome Agate';
 
-        // let articleModel = new Article();
-
-        this.scope.articles = Article.query()
-                                .skipUndefined()
-                                .orderBy('id')
-                                .limit(10);
-
-        this.scope.articles.then((row: any) => {
-            console.log(row);
+        await Article.query().orderBy('id').limit(10).then((articles: Array<Article>) => {
+            this.scope.articles = articles.map((article: Article) => { return article.title });
         });
+
+        console.log(this.scope.articles);
     }
 }
--- app/models/article.ts
+++ app/models/article.ts
@@ -3,7 +3,10 @@ import ActiveMode from "../../lib/agate/active_model";
 export default class Article extends ActiveMode {
     protected static modelName = 'articles';
 
-    constructor() {
-        super();
+    constructor(public name: string)
+        { super(); }
+
+    public get title(): string {
+        return this.name;
     }
 }
